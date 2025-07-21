21b7544aebf232421c34734541eb14955e9cd176
Carsten Gips <cagix@hsbi.de>
2025-07-21 18:56:20 +0200

    origin: do analyse doc.blocks only
    There are certain cases, were we do have something defined in the
    YAML header but do not use this in the document or for an output
    format - in this case the filter will find this as well and put it
    as exception to the license.
    
    Change the behaviour: Do work on doc.blocks only, after the
    document structure has been assembled.
    
    fixes #37

--- scripts/elements/origin.lua
+++ scripts/elements/origin.lua
@@ -1,58 +1,65 @@
 -- Collect all 'origin' spans - this is foreign material, i.e. should be listed as exceptions to our license
 credits = {}
 
-function Span(el)
-    -- Collect all 'origin' spans
-    if el.classes[1] == "origin" then
-        -- use map to avoid duplicates
-        -- (when used in images, this would end up in alt text _and_ in caption)
-        credits[pandoc.utils.stringify(el.content)] = el.content
-
-        -- add "Quelle: " in front of content
-        if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
-            return {
-                pandoc.RawInline('latex', '\\origin{Quelle: '),
-                pandoc.Span(el.content),
-                pandoc.RawInline('latex', '}')
-            }
-        end
-        if FORMAT:match ('gfm') or (FORMAT:match 'markdown') then
-            return { pandoc.Str("Quelle: ") } .. el.content
-        end
-        io.stderr:write("\t (origin) unexpected format: '" .. FORMAT .. "' ... \n")
-    end
-end
-
 
 --- Fetch all exceptions from our license and replace the custom marker
-function Div(el)
-    if el.classes[1] == "exceptions" then
-        local bullets = pandoc.List()
+exceptions = {
+    Span = function(el)
+        -- Collect all 'origin' spans
+        if el.classes[1] == "origin" then
+            -- use map to avoid duplicates
+            -- (when used in images, this would end up in alt text _and_ in caption)
+            credits[pandoc.utils.stringify(el.content)] = el.content
 
-        -- fetch all exceptions into bullet points
-        for _, v in pairs(credits) do
-            bullets:insert(pandoc.Plain(v))
-        end
-
-        if #bullets > 0 then
-            -- we do have some exceptions
+            -- add "Quelle: " in front of content
             if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
                 return {
-                    pandoc.RawBlock('latex', '\\bigskip'),
-                    pandoc.Plain(pandoc.Strong('Exceptions:')),
-                    pandoc.BulletList(bullets)
+                    pandoc.RawInline('latex', '\\origin{Quelle: '),
+                    pandoc.Span(el.content),
+                    pandoc.RawInline('latex', '}')
                 }
             end
             if FORMAT:match ('gfm') or (FORMAT:match 'markdown') then
-                return {
-                    pandoc.Plain(pandoc.Strong('Exceptions:')),
-                    pandoc.BulletList(bullets)
-                }
+                return { pandoc.Str("Quelle: ") } .. el.content
             end
             io.stderr:write("\t (origin) unexpected format: '" .. FORMAT .. "' ... \n")
-        else
-            -- nope, nothing ...
-            return {}  -- remove marker anyway
+        end
+    end,
+
+    Div = function(el)
+        if el.classes[1] == "exceptions" then
+            local bullets = pandoc.List()
+
+            -- fetch all exceptions into bullet points
+            for _, v in pairs(credits) do
+                bullets:insert(pandoc.Plain(v))
+            end
+
+            if #bullets > 0 then
+                -- we do have some exceptions
+                if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
+                    return {
+                        pandoc.RawBlock('latex', '\\bigskip'),
+                        pandoc.Plain(pandoc.Strong('Exceptions:')),
+                        pandoc.BulletList(bullets)
+                    }
+                end
+                if FORMAT:match ('gfm') or (FORMAT:match 'markdown') then
+                    return {
+                        pandoc.Plain(pandoc.Strong('Exceptions:')),
+                        pandoc.BulletList(bullets)
+                    }
+                end
+                io.stderr:write("\t (origin) unexpected format: '" .. FORMAT .. "' ... \n")
+            else
+                -- nope, nothing ...
+                return {}  -- remove marker anyway
+            end
         end
     end
+}
+
+-- just analyse the (final) document structure to avoid including unused meta data
+function Pandoc(doc)
+    return pandoc.Pandoc(doc.blocks:walk(exceptions), doc.meta)
 end
