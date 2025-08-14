3d04c73ce3f22fbe45be1c508010fd6d037521df
Carsten Gips <cagix@hsbi.de>
2025-08-14 18:27:20 +0200

    refactoring: extract creating of alerts into common function

--- scripts/elements/ghalerts.lua
+++ scripts/elements/ghalerts.lua
@@ -1,45 +1,50 @@
 -- GitHub Alerts: replace alert Divs with "real" GH alerts
 
+-- GH Alerts to be handled
+local alerts = {
+    note = {
+        tex  = "info-box",
+        gfm  = "[!NOTE]",
+    },
+    tip = {
+        tex  = "tip-box",
+        gfm  = "[!TIP]",
+    },
+    important = {
+        tex  = "important-box",
+        gfm  = "[!IMPORTANT]",
+    },
+    warning = {
+        tex  = "warning-box",
+        gfm  = "[!WARNING]",
+    },
+    caution = {
+        tex  = "error-box",
+        gfm  = "[!CAUTION]",
+    },
+}
+
+local function makeAlert(type, content)
+    if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
+        return {
+                pandoc.RawBlock('latex', '\\begin{' .. type ..'}'),
+                pandoc.Div(content),
+                pandoc.RawBlock('latex', '\\end{' .. type .. '}')
+            }
+    end
+
+    if (FORMAT:match 'gfm') or (FORMAT:match 'markdown') then
+        return pandoc.BlockQuote({pandoc.RawBlock("markdown", type)} .. content)
+    end
+end
+
+
 if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
     function Div(el)
-        if el.classes[1] == "note" then
-            return {
-                pandoc.RawBlock('latex', '\\begin{info-box}'),
-                pandoc.Div(el.content),
-                pandoc.RawBlock('latex', '\\end{info-box}')
-            }
-        end
+        local type = el.classes[1]
 
-        if el.classes[1] == "tip" then
-            return {
-                pandoc.RawBlock('latex', '\\begin{tip-box}'),
-                pandoc.Div(el.content),
-                pandoc.RawBlock('latex', '\\end{tip-box}')
-            }
-        end
-
-        if el.classes[1] == "important" then
-            return {
-                pandoc.RawBlock('latex', '\\begin{important-box}'),
-                pandoc.Div(el.content),
-                pandoc.RawBlock('latex', '\\end{important-box}')
-            }
-        end
-
-        if el.classes[1] == "warning" then
-            return {
-                pandoc.RawBlock('latex', '\\begin{warning-box}'),
-                pandoc.Div(el.content),
-                pandoc.RawBlock('latex', '\\end{warning-box}')
-            }
-        end
-
-        if el.classes[1] == "caution" then
-            return {
-                pandoc.RawBlock('latex', '\\begin{error-box}'),
-                pandoc.Div(el.content),
-                pandoc.RawBlock('latex', '\\end{error-box}')
-            }
+        if alerts[type] then
+            return makeAlert(alerts[type].tex, el.content)
         end
     end
 end
@@ -47,40 +52,20 @@ end
 
 if (FORMAT:match 'gfm') or (FORMAT:match 'markdown') then
     function Div(el)
-        if el.classes[1] == "note" then
-            return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!NOTE]')} .. el.content)
-        end
+        local type = el.classes[1]
 
-        if el.classes[1] == "tip" then
-            return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!TIP]')} .. el.content)
-        end
-
-        if el.classes[1] == "warning" then
-            return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!WARNING]')} .. el.content)
-        end
-
-        if el.classes[1] == "important" then
-            if FORMAT:match 'markdown' then
-                -- docsify: "important" not supported in docsify
-                io.stderr:write("[WARNING]  [ghalerts.lua]  GitHub alert 'important' not supported in docsify (replacing w/ quote)\n")
+        -- handle these two alerts differently when generating for docsify
+        -- docsify: "important" and "caution" not supported in docsify
+        if FORMAT:match 'markdown' then
+            if (type == "important") or (type == "caution") then
+                io.stderr:write("[WARNING]  [ghalerts.lua]  GitHub alert '" .. type .. "' not supported in docsify (replacing w/ quote)\n")
                 return pandoc.BlockQuote(el.content)
-            else
-                -- gfm
-                return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!IMPORTANT]')} .. el.content)
             end
         end
 
-        if el.classes[1] == "caution" then
-            if FORMAT:match 'markdown' then
-                -- docsify: "caution" not supported in docsify
-                io.stderr:write("[WARNING]  [ghalerts.lua]  GitHub alert 'caution' not supported in docsify (replacing w/ quote)\n")
-                return pandoc.BlockQuote(el.content)
-            else
-                -- gfm
-                return pandoc.BlockQuote({pandoc.RawBlock("markdown", '[!CAUTION]')} .. el.content)
-            end
+        -- default: emit proper gh alert
+        if alerts[type] then
+            return makeAlert(alerts[type].gfm, el.content)
         end
     end
 end
-
-
