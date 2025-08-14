b7a143ac2797d0aece7055a804d5527dabea9f4f
Carsten Gips <cagix@hsbi.de>
2025-08-14 16:55:27 +0200

    templates: refactor common methods for wrapping custom divs into separate filter

--- scripts/elements/details.lua
+++ scripts/elements/details.lua
@@ -6,7 +6,7 @@ if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
         if el.classes[1] == "details" then
             local bl = pandoc.List()
 
-            bl:insert(pandoc.RawBlock('latex', '\\' .. (el.attributes["fontsize"] or "normalsize")))
+            bl:insert(pandoc.RawBlock('latex', '\\' .. (el.attributes["opt"] or "normalsize")))
 
             if el.attributes["title"] then
                 bl:insert(pandoc.Plain(pandoc.Strong(el.attributes["title"])))
@@ -27,11 +27,13 @@ if (FORMAT:match 'gfm') or (FORMAT:match 'markdown') then
         if el.classes[1] == "details" then
             local bl = pandoc.List()
 
-            bl:insert(pandoc.RawBlock("markdown", '<details' .. (el.attributes["open"] or "") .. '>'))
+            bl:insert(pandoc.RawBlock("markdown", '<details' .. (el.attributes["opt"] or "") .. '>'))
+
             if el.attributes["title"] then
                 bl:insert(pandoc.RawBlock("markdown", '<summary><strong>' .. el.attributes["title"] .. '</strong></summary>'))
             end
             bl:extend(el.content)
+
             bl:insert(pandoc.RawBlock("markdown", '</details>'))
 
             return bl
