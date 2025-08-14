cfb83a13830437aa40b31682680d3fbeb0502ba9
Carsten Gips <cagix@hsbi.de>
2025-08-14 11:14:37 +0200

    transform: refactor filter and extract helper function

--- scripts/document/format.lua
+++ scripts/document/format.lua
@@ -1,49 +1,32 @@
 -- "Template" for (re-)formatting documents properly
+-- custom divs are expected to be found in meta data (divtometa.lua)
 
 --- Structure of the document (should be done w/ template, but quotes won't work)
 function Pandoc(doc)
-    local blocks = pandoc.List()
+    local blocks = pandoc.List
 
-    -- TL;DR and Videos
-    if doc.meta.tldr then
-        blocks:insert(pandoc.Div(doc.meta.tldr, {class = 'tldr'}))
-        doc.meta.tldr = nil
+    local handle = function(div)
+        if doc.meta[div] then
+            blocks:insert(pandoc.Div(doc.meta[div], {class = div}))
+            doc.meta[div] = nil
+        end
     end
 
-    if doc.meta.youtube then
-        blocks:insert(pandoc.Div(doc.meta.youtube, {class = 'youtube'}))
-        doc.meta.youtube = nil
-    end
-
-    if doc.meta.attachments then
-        blocks:insert(pandoc.Div(doc.meta.attachments, {class = 'attachments'}))
-        doc.meta.attachments = nil
-    end
+    -- TL;DR and Videos and Files
+    handle("tldr")
+    handle("youtube")
+    handle("attachments")
 
     -- Main Doc
     blocks:extend(doc.blocks)
 
     -- Literature
-    if doc.meta.readings then
-        blocks:insert(pandoc.Div(doc.meta.readings, {class = 'readings'}))
-        doc.meta.readings = nil
-    end
+    handle("readings")
 
     -- Outcomes, Quizzes, and Challenges
-    if doc.meta.outcomes then
-        blocks:insert(pandoc.Div(doc.meta.outcomes, {class = 'outcomes'}))
-        doc.meta.outcomes = nil
-    end
-
-    if doc.meta.quizzes then
-        blocks:insert(pandoc.Div(doc.meta.quizzes, {class = 'quizzes'}))
-        doc.meta.quizzes = nil
-    end
-
-    if doc.meta.challenges then
-        blocks:insert(pandoc.Div(doc.meta.challenges, {class = 'challenges'}))
-        doc.meta.challenges = nil
-    end
+    handle("outcomes")
+    handle("quizzes")
+    handle("challenges")
 
 
     -- Finé
