8f28103abd6b95e70366e36d08a2a746b4b86bf0
Carsten Gips <cagix@hsbi.de>
2026-03-26 16:44:48 +0100

    ghalerts: refactor filter to improve readability

--- scripts/elements/ghalerts.lua
+++ scripts/elements/ghalerts.lua
@@ -31,35 +31,29 @@ local function _is_genuine_ghalert(c)
 end
 
 
-if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
-    function Div(el)
-        local type = el.classes[1]
+function Div(el)
+    local type = el.classes[1]
 
-        if alerts[type] then
-            local title = el.attributes["title"] and ("[" .. el.attributes["title"] .. "]") or ""
-            local c = el.content
+    if alerts[type] then
+        local c = el.content
+
+        if (FORMAT:match 'latex') or (FORMAT:match 'beamer') then
             if _is_genuine_ghalert(c) then
                 c:remove(1) -- remove title div from original ghalert
             end
+            local title = el.attributes["title"] and ("[" .. el.attributes["title"] .. "]") or ""
+            local box = alerts[type].tex
             return {
-                pandoc.RawBlock('latex', '\\begin{' .. alerts[type].tex ..'}' .. title),
+                pandoc.RawBlock('latex', '\\begin{' .. box ..'}' .. title),
                 pandoc.Div(c),
-                pandoc.RawBlock('latex', '\\end{' .. alerts[type].tex .. '}')
+                pandoc.RawBlock('latex', '\\end{' .. box .. '}')
             }
         end
-    end
-end
 
-
-if FORMAT:match 'markdown' then
-    function Div(el)
-        local type = el.classes[1]
-
-        -- default: emit proper gh alert
-        if alerts[type] then
-            local c = el.content
+        if FORMAT:match 'markdown' then
             -- do not touch genuine GH alerts
             if not _is_genuine_ghalert(c) then
+                -- emit proper gh alert
                 -- TODO workaround: the markdown writer would not recognize the gh-alert properly if there isn't plain text at the beginning (see https://github.com/jgm/pandoc/issues/11533)
                 el.content = { pandoc.Div(alerts[type].md, {class='title'}), pandoc.Para(pandoc.Str("")) } .. c
                 return el
